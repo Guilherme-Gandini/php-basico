28785825a8c90e52a02595272a7da2aaa6404aeb
Guilherme Gandini <guilherme.gandini@aluno.senai.br>
2025-10-10 16:32:40 -0300

    trabalhos feitos
    Ex 1-4 feitos

---
+++ aula1/exemplo1.php
@@ -0,0 +1,12 @@
+<?php
+    // Geração de de texto (string)
+    echo "Chama Sesi-Senai!";
+    
+    /*Geração de texto estruturado com tags e atributos*/
+
+    //Aspas simples dentro de aspas duplas
+    echo "<h2>Gerando <spam class='destaque'>HTML</spam> através do PHP.</h2>";
+
+    //Aspas duplas duplas dentro de aspas duplas
+    echo "<h2>Gerando <spam class=\*destaque\*>HTML</spam> através do PHP.</h2>";
+?>
